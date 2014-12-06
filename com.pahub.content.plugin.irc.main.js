a35e9807e3d4e77f75b02553c8cf951cc639e3e8
domdom <domdom314@gmail.com>
2014-12-06 14:13:57 +1000

    Update main to use color module

--- com.pahub.content.plugin.irc.main.js
+++ com.pahub.content.plugin.irc.main.js
@@ -1,11 +1,10 @@
 function load_plugin_irc(data, folder) {
     var irc = require(path.join(folder, 'node_modules', 'irc'));
-    model["irc"] = new irc_view_model(irc);
-    
-
+    var color = require(path.join(folder, 'node_modules', 'randomcolor'));
+    model["irc"] = new irc_view_model(irc, color);
 
     pahub.api.resource.loadResource(path.join(folder, "index.html"), "get", {name: "HTML: irc", mode: "async", success: function(resource) {
-        pahub.api.tab.addTab("section-community", "irc", "#IRC", null, 11);
+        pahub.api.tab.addTab("section-community", "irc", "#IRC", "", 15);
         pahub.api.tab.setTabContent("section-community", "irc", resource.data);
     }});
 }
